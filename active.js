76c49b09dcf1024604a4e76ab7931cc8dea093b4
aterlecka <aleksandra.terlecka1996@gmail.com>
2021-06-10 18:47:10 +0200

    create simple menu

---
+++ active.js
@@ -0,0 +1,19 @@
+
+document.addEventListener("click", function(){
+    transform()
+});
+
+function transform (){
+document.getElementById("element").style.display="block";
+
+setTimeout(function(){	document.getElementById("element").style.display="none";},14000);
+
+};
+
+
+
+
+
+
+
+
